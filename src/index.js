47c73dcb591593817fd402b63dd8e720ca45e529
Luis Jose Marcano Michelena <91623688+Luijom98@users.noreply.github.com>
2021-12-11 11:18:47 -0600

    Functional components my firts functional component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello world</h1>, document.getElementById("root"));
+function MyInfo() {
+  return (
+    <div>
+      <h1>Luis Jose</h1>
+      <p>I like coding, exercice and chess</p>
+      <ol>
+        <li>Spain</li>
+        <li>Amsterdam</li>
+        <li>Italy</li>
+      </ol>
+    </div>
+  );
+}
+
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
