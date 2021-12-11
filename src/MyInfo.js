8a6810b8ce044a26ab43c5dec64bf360d36ee15a
Luis Jose Marcano Michelena <91623688+Luijom98@users.noreply.github.com>
2021-12-11 11:34:51 -0600

    Functional Component My firts functional component

---
+++ src/MyInfo.js
@@ -0,0 +1,15 @@
+function MyInfo() {
+  return (
+    <div>
+      <h1>Luis Jose</h1>
+      <p>I like coding, exercice and chess</p>
+      <ol>
+        <li>Spain</li>
+        <li>Amsterdam</li>
+        <li>Italy</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyInfo;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-function MyInfo() {
-  return (
-    <div>
-      <h1>Luis Jose</h1>
-      <p>I like coding, exercice and chess</p>
-      <ol>
-        <li>Spain</li>
-        <li>Amsterdam</li>
-        <li>Italy</li>
-      </ol>
-    </div>
-  );
-}
+import MyInfo from "./MyInfo";
 
 ReactDOM.render(<MyInfo />, document.getElementById("root"));
